dc458057b5d066235b8f20c9f504c33dbe08d951
miheyinfo <miheyinfo@gmail.com>
2021-07-10 14:58:07 +0200

    add mapping for all files

--- coherentPointDrift.py
+++ coherentPointDrift.py
@@ -17,6 +17,22 @@ def visualize(iteration, error, X, Y, ax):
     plt.draw()
     plt.pause(0.001)
 
+
+def createSimplePlot(figure, char, color):
+    x = []
+    y = []
+    for item in figure:
+        x.append(item[0])
+        y.append(item[1])
+    plt.plot(x, y, char, color=color)
+    return plt
+
+def plotTwoFigures(X, Y):
+    createSimplePlot(X, 'o', 'green')
+    createSimplePlot(Y, 'o', 'black')
+    plt.draw()
+    plt.pause(0.001)
+
 def plotContourCoordinates(contour_coordinates):
     x = []
     y = []
@@ -37,8 +53,25 @@ def getCoordinatesFromObjFile(fileName):
     for line in Lines:
         splitedLine = line.strip().split()
         if splitedLine[0] == "v":
-            coordinates.append([(float(splitedLine[1])+14)*6000, (float(splitedLine[3])+20)*4667])
+            coordinates.append([(float(splitedLine[1])), (float(splitedLine[3]))])
     return np.array(coordinates)
+
+def getContourCoordinatesFromJSONFile(fileName):
+    with open(fileName, 'r') as jsonFile:
+        data = jsonFile.read()
+
+    # parse file
+    jsonObject = json.loads(data)
+
+    info_array = jsonObject['INFO']
+    ## get contour
+    contour = list(filter(lambda x: x["properties"]["classification"]["name"] == "Region*", info_array))
+    contour_coordinates = contour[0]["geometry"]["coordinates"][0]
+    for item in contour_coordinates:
+        item[0] = float(item[0])/10000
+        item[1] = float(item[1])/10000
+    return contour_coordinates
+
 
 def rotate(data, theta):
     ox, oy = 0, 0  # point to rotate about
@@ -52,45 +85,29 @@ def rotate(data, theta):
     return w
 
 def main():
-    # X = np.loadtxt('test.txt')
-    # plotContourCoordinates(X)
-    coordinatesFromObj = getCoordinatesFromObjFile('C.obj')
-    # print(coordinatesFromObj)
-    #plotContourCoordinates(coordinatesFromObj)
 
-    # read file
-    with open('FALL-Y-1-C_GeoJSON.json', 'r') as jsonFile:
-        data = jsonFile.read()
+    objectNames = ['C','D','E','F','G','H']
+    for name in objectNames:
+        coordinatesFromObj = getCoordinatesFromObjFile(name + '.obj')
+        coordinatesFromJSON = np.array(getContourCoordinatesFromJSONFile('FALL-Y-1-' + name + '_GeoJSON.json'))
 
-    # parse file
-    jsonObject = json.loads(data)
+        # if name == 'C':
+        #     coordinatesFromJSON = rotate(coordinatesFromJSON, -1.66)
+        # #if name == 'D':
+        # # neeed to edit source qpath file
+        # if name == 'E':
+        #     coordinatesFromJSON = rotate(coordinatesFromJSON, 3.1)
+        # if name == 'F':
+        #     coordinatesFromJSON = rotate(coordinatesFromJSON, 3.5)
+        # if name == 'H':
+        #     coordinatesFromJSON = rotate(coordinatesFromJSON, 3.1)
 
-    info_array = jsonObject['INFO']
-    ## get contour
-    contour = list(filter(lambda x: x["properties"]["classification"]["name"] == "Region*", info_array))
-    contour_coordinates = np.array(contour[0]["geometry"]["coordinates"][0])
-    #plotContourCoordinates(contour_coordinates)
-    #
+        # plotTwoFigures(coordinatesFromJSON, coordinatesFromObj)
 
+        reg = AffineRegistration(**{'X': np.array(coordinatesFromObj), 'Y': np.array(coordinatesFromJSON)})
+        poly_wsi_reg = reg.register()[0]
 
-    contour_coordinates = rotate(contour_coordinates, -1.66)
-
-    for item in contour_coordinates:
-        item[1] += 120000
-
-    #plotContourCoordinates(contour_coordinates)
-
-    Y = contour_coordinates
-    X = coordinatesFromObj
-
-    reg = AffineRegistration(**{'X': X, 'Y': Y})
-    poly_wsi_reg = reg.register()[0]
-
-    plotContourCoordinates(contour_coordinates)
-    plotContourCoordinates(coordinatesFromObj)
-    plotContourCoordinates(poly_wsi_reg)
-
-    plt.show()
+        plotTwoFigures(coordinatesFromObj, poly_wsi_reg)
 
 
 if __name__ == '__main__':
